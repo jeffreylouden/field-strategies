ab73f91f29954e38f4ffb0d2a5d2e66149b50749
Jeffrey Louden <jeff@jefflouden.com>
2013-03-25 08:48:59 -0400

    cleaning up for client presentation

--- js/core.js
+++ js/core.js
@@ -2,8 +2,12 @@
     "use strict";
 
     var
+        ACTIVE          =   'active',
         CURRENT         =   'current',
+        ID              =   'id',
         NEXT            =   'next',
+        OFFSETBOTTOM    =   'data-offset-bottom',
+        OFFSETTOP       =   'data-offset-top',
         PAST            =   'past',
         PREV            =   'previous',
         SHOW            =   'show',
@@ -11,17 +15,25 @@
         html            =   $('html'),
         htmlbody        =   html.add('body'),
         nav             =   $('nav'),
+        navLogo         =   $(doc.getElementById('nav-logo')),
         navItem         =   nav.find('a'),
         mobileNav       =   $(doc.getElementById('mobile-nav')),
         homeLink        =   mobileNav.find('b'),
         navLink         =   mobileNav.find('i'),
 
+        header          =   $(doc.getElementById('hd')),
+        intro          =   $(doc.getElementById('intro')),
+        contact         =   $(doc.getElementById('contact')),
+
+        main            =   $(doc.getElementById('main')),
+        sections        =   main.find('article'),
+        scrollDistance,
+        currentSection,
+
         testimonials    =   $(doc.getElementById('testimonials')),
         quotes          =   testimonials.find('.quote'),
         currentSlide,
         currentIndex;
-
-        console.log(quotes);
 
     function cycle(){
         switchSlide(NEXT);
@@ -65,8 +77,6 @@
 
     function scrollToSection( target ) {
 
-        console.log(target);
-
         if ( target === null || typeof target === 'undefined') {
             htmlbody.animate({scrollTop:  0}, 600);
         } else {
@@ -74,13 +84,82 @@
                 s                       =   $(target),
                 targetURL               =   win.location.origin + '/' + target,
                 elementDistanceFromTop  =   s.offset().top;
-            htmlbody.animate({scrollTop: elementDistanceFromTop - 49}, 600);
+            htmlbody.animate({scrollTop: elementDistanceFromTop - 56}, 600);
         }
 
         // try { win._gaq.push([TRACKEVENT, ZACH, GASCROLL, target.attr(ID)]); } catch(e) {}
     }
 
+    function sectionHighlight( scrollPosition ) {
+        sections.each(function(){
+            var
+                t               =   $(this),
+                sectionId       =   t.attr(ID);
+            if ( (scrollPosition >= t.attr(OFFSETTOP)) && ( scrollPosition < t.attr(OFFSETBOTTOM)) && ( sectionId !== currentSection) ) {
+
+                if (!navLogo.hasClass(SHOW)) {
+                    navLogo.addClass(SHOW);
+                }
+
+                currentSection = sectionId;
+
+                switch (currentSection) {
+                    case 'testimonials':
+                        currentSection = 'clients';
+                        break;
+                    case 'full-list':
+                        currentSection = 'services';
+                        break;
+                }
+
+                nav.find('.active').removeClass(ACTIVE);
+                $(nav.find('[href=#' + currentSection + ']').closest('li')).addClass(ACTIVE);
+
+            } else {
+                if ( scrollPosition <= header.attr(OFFSETBOTTOM)) {
+                        nav.find('.active').removeClass(ACTIVE);
+                        navLogo.removeClass(SHOW);
+                        currentSection = '';
+                }
+                if ( scrollPosition >= contact.attr(OFFSETTOP)) {
+                    currentSection = 'contact';
+                    nav.find('.active').removeClass(ACTIVE);
+                    $(nav.find('[href=#contact]').closest('li')).addClass(ACTIVE);
+                }
+            }
+        });
+    }
+
+    function setSections() {
+        sections.each(function() {
+            var s                 =   $(this);
+            s.attr(OFFSETTOP, Math.floor(s.offset().top) - 56);
+            s.attr(OFFSETBOTTOM, parseInt(s.attr(OFFSETTOP),10) + parseInt(s.outerHeight(true), 10));
+        });
+
+        contact.attr(OFFSETTOP, Math.floor(contact.offset().top) - 56);
+        contact.attr(OFFSETBOTTOM, parseInt(contact.attr(OFFSETTOP),10) + parseInt(contact.outerHeight(true), 10));
+
+        header.attr(OFFSETTOP, Math.floor(header.offset().top) - 56);
+        header.attr(OFFSETBOTTOM, parseInt(header.attr(OFFSETTOP),10) + parseInt((header.outerHeight(true) + intro.outerHeight(true)), 10));
+    }
+
+    function init() {
+        cycle();
+        quotes.eq(0).addClass(CURRENT);
+
+        setSections();
+    }
+
     homeLink.on('click', function(e) {
+        e.preventDefault();
+        scrollToSection();
+        if (nav.hasClass(SHOW)) {
+            nav.removeClass(SHOW);   
+        }
+    });
+
+    navLogo.on('click', function(e) {
         e.preventDefault();
         scrollToSection();
         if (nav.hasClass(SHOW)) {
@@ -100,7 +179,19 @@
         nav.toggleClass(SHOW);
     });
 
-    cycle();
-    quotes.eq(0).addClass(CURRENT);
+    $(win).resize(function(){
+        setSections();
+    });
+
+    $(win).scroll(function(){
+
+        scrollDistance  =   $(this).scrollTop();
+        scrollDistance  =   scrollDistance ? scrollDistance : 0;
+
+        sectionHighlight(scrollDistance);
+    })
+
+    init();
 
 })(jQuery, window, document);
+
